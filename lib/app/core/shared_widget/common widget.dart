cddf906c8fa4c216565648e131c9b7f97885be8f
Sourav . <sourav@birlasoft.com>
2024-02-21 16:35:03 +0530

    login screen partiali done

--- lib/app/core/shared_widget/common widget.dart
+++ lib/app/core/shared_widget/common widget.dart
@@ -1,10 +1,29 @@
-
 import 'package:flutter/material.dart';
 import 'package:smart_search/app/core/responsive/responsive_sizer/responsive_sizer.dart';
 import 'package:smart_search/app/core/utils/text_style.dart';
 import '../responsive/responsive_width.dart';
 import '../utils/colors.dart';
 
+Widget buttonWidget({
+  required String text,
+  required Color buttonColor,
+  required Function() onTap,
+  Color? textColor,
+}) {
+  return ElevatedButton(
+    onPressed: () {},
+    style: ElevatedButton.styleFrom(
+        backgroundColor: buttonColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        textStyle: CustomTextStyle.textStyle500(
+            color: textColor,
+            fontSize: widgetSize(desktop: 14.sp, tablet: 15.sp, mobile: 17.sp),
+            fontStyle: FontStyle.normal)),
+    child: Text(text),
+  );
+}
 
 Widget customButton({
   required String text,
@@ -18,7 +37,7 @@ Widget customButton({
       decoration: BoxDecoration(
         color: buttonColor,
         borderRadius: BorderRadius.all(
-          Radius.circular(10.sp),
+          Radius.circular(5.sp),
         ),
       ),
       padding: EdgeInsets.symmetric(
@@ -36,8 +55,7 @@ Widget customButton({
   );
 }
 
-
-Widget errorWidget({required String errorMsg}){
+Widget errorWidget({required String errorMsg}) {
   return Padding(
     padding: EdgeInsets.symmetric(
       vertical: 20.h,
@@ -52,11 +70,7 @@ Widget errorWidget({required String errorMsg}){
   );
 }
 
-
-
-
-
-Widget loginErrorWidget({required String errorMsg}){
+Widget loginErrorWidget({required String errorMsg}) {
   return Column(
     mainAxisAlignment: MainAxisAlignment.center,
     crossAxisAlignment: CrossAxisAlignment.center,
@@ -76,19 +90,14 @@ Widget loginErrorWidget({required String errorMsg}){
       customButton(
         text: "Login",
         buttonColor: CustomColors.buttonBlueColor,
-        onTap: () {
-         
-        },
+        onTap: () {},
         textColor: CustomColors.whiteColor,
       ),
     ],
   );
 }
 
-
-
-
-Widget loaderWidget({EdgeInsets? padding}){
+Widget loaderWidget({EdgeInsets? padding}) {
   return Padding(
     padding: padding ?? EdgeInsets.symmetric(vertical: 40.h, horizontal: 40.w),
     child: const Center(
--- lib/app/features/auth/presentation/pages/login.dart
+++ lib/app/features/auth/presentation/pages/login.dart
@@ -1,40 +1,130 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:smart_search/app/core/mixins/validation_mixin.dart';
+
 import 'package:smart_search/app/core/responsive/responsive_sizer/responsive_sizer.dart';
+import 'package:smart_search/app/core/shared_widget/common%20widget.dart';
 import 'package:smart_search/app/core/utils/colors.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class Login extends StatelessWidget with FormValidationMixin {
+  Login({super.key});
+
+  final GlobalKey _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passController = TextEditingController();
+
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
-    return ResponsiveSizer(
-        builder: (context, orientation, screenType) {
-          return SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 1.h, horizontal: 0.w),
-                  child: Image.asset(
-                    'assets/images/Picture2.jpg',
-                    alignment: Alignment.center,
-                    height: 20.h,
-                    width: 90.w,
-                    scale: 2.5,
-                    fit: BoxFit.cover,
-                  ),
-                ), //Image.asset
-                Container(
-                  height: 80.h,
+    return Form(
+      key: _formKey,
+      child: ResponsiveSizer(
+          builder: (context, orientation, screenType) {
+            return SingleChildScrollView(
+              child: Material(
+                child: Container(
+                  height: 100.h,
                   width: 100.w,
                   color: CustomColors.appSecondryColor,
-                )
-              ],
-            ),
-          );
-        },
-        context: context);
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 100.w,
+                        color: CustomColors.whiteColor,
+                        padding: EdgeInsets.symmetric(
+                            vertical: 1.h, horizontal: 1.5.w),
+                        child: Image.asset(
+                          'assets/images/part_of_truck_image.png',
+                          alignment: Alignment.center,
+                          height: 20.h,
+                          width: 96.w,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 15.h,
+                      ),
+                      TextFormField(
+                        controller: _emailController,
+                        focusNode: _emailFocusNode,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                          labelText: 'Email',
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: validateEmail, // Validating email
+                        onFieldSubmitted: (value) {
+                          _emailFocusNode.unfocus();
+                          FocusScope.of(context)
+                              .requestFocus(_passwordFocusNode);
+                        },
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        controller: _passController,
+                        focusNode: _passwordFocusNode,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          labelText: 'Password',
+                          border: OutlineInputBorder(),
+                          errorMaxLines: 3,
+                        ),
+                        validator: validatePassword, // Validating password
+                        onFieldSubmitted: (value) {
+                          _passwordFocusNode.unfocus();
+                        },
+                      ),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      Container(
+                        width: 100.w,
+                        color: CustomColors.appSecondryColor,
+                        padding: EdgeInsets.symmetric(
+                            vertical: 1.h, horizontal: 1.5.w),
+                        child: Image.asset(
+                          'assets/images/eng_fr_logo.jpg',
+                          alignment: Alignment.center,
+                          height: 10.h,
+                          width: 15.w,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5.h,
+                      ),
+                      Center(
+                          child: buttonWidget(
+                              onTap: () {
+                                // if (_formKey.currentState!.validate()) {
+                                //   ScaffoldMessenger.of(context).showSnackBar(
+                                //     const SnackBar(content: Text('Thank you!')),
+                                //   );
+                                // }
+                              },
+                              text: "Login",
+                              buttonColor: CustomColors.appPrimaryColor,
+                              textColor: CustomColors.whiteColor)),
+                      SizedBox(
+                        height: 10.h,
+                      ),
+                      Center(
+                          child: buttonWidget(
+                              onTap: () {},
+                              text: "Sign Up",
+                              buttonColor: CustomColors.appSecondryColor,
+                              textColor: CustomColors.whiteColor)),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+          context: context),
+    );
   }
 }
