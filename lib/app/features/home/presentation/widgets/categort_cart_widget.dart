9adcc6110192ee700a286dc5fb9c40a86548b409
Keshav Taskar <keshavt@birlasoft.com>
2024-02-22 10:46:27 +0530

    Created scan part page

--- lib/app/features/home/presentation/widgets/categort_cart_widget.dart
+++ lib/app/features/home/presentation/widgets/categort_cart_widget.dart
@@ -5,6 +5,7 @@ import 'package:smart_search/app/core/responsive/responsive_sizer/responsive_siz
 import 'package:smart_search/app/core/responsive/responsive_width.dart';
 import 'package:smart_search/app/core/utils/colors.dart';
 import 'package:smart_search/app/core/utils/text_style.dart';
+import 'package:smart_search/app/features/scan_part/presentation/pages/scan_part.dart';
 
 class CategoryCartWidget extends StatelessWidget {
   CategoryModel model;
@@ -12,45 +13,51 @@ class CategoryCartWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 40.w,
-      // padding: EdgeInsets.all(1.w),
-      decoration: const BoxDecoration(
-          color: CustomColors.appPrimaryColor,
-          // shape: BoxShape.circle, // BoxShape.circle or BoxShape.retangle
-          //color: const Color(0xFF66BB6A),
-          boxShadow: [
-            BoxShadow(
-                color: CustomColors.blackColor,
-                blurRadius: 7.0,
-                offset: Offset(0.0, 0.75)),
-          ]),
-      margin: EdgeInsets.all(2.w),
-      child: Column(
-        children: [
-          Image.asset(
-            model.image,
-            width: 35.w,
-            height: 35.w,
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 1.w),
-            height: 6.h,
-            alignment: Alignment.bottomCenter,
-            child: Text(
-              model.title.toUpperCase(),
-              maxLines: 5,
-              textAlign: TextAlign.center,
-              style: CustomTextStyle.textStyle500(
-                  color: CustomColors.whiteColor,
-                  fontSize:
-                      widgetSize(desktop: 14.sp, tablet: 16.sp, mobile: 14.sp)),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const ScanPart()));
+      },
+      child: Container(
+        width: 40.w,
+        // padding: EdgeInsets.all(1.w),
+        decoration: const BoxDecoration(
+            color: CustomColors.appPrimaryColor,
+            // shape: BoxShape.circle, // BoxShape.circle or BoxShape.retangle
+            //color: const Color(0xFF66BB6A),
+            boxShadow: [
+              BoxShadow(
+                  color: CustomColors.blackColor,
+                  blurRadius: 7.0,
+                  offset: Offset(0.0, 0.75)),
+            ]),
+        margin: EdgeInsets.all(2.w),
+        child: Column(
+          children: [
+            Image.asset(
+              model.image,
+              width: 35.w,
+              height: 35.w,
             ),
-          ),
-          SizedBox(
-            height: 2.h,
-          ),
-        ],
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 1.w),
+              height: 6.h,
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                model.title.toUpperCase(),
+                maxLines: 5,
+                textAlign: TextAlign.center,
+                style: CustomTextStyle.textStyle500(
+                    color: CustomColors.whiteColor,
+                    fontSize: widgetSize(
+                        desktop: 14.sp, tablet: 16.sp, mobile: 14.sp)),
+              ),
+            ),
+            SizedBox(
+              height: 2.h,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/app/features/scan_part/presentation/pages/scan_part.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:smart_search/app/core/responsive/responsive_sizer/responsive_sizer.dart';
+import 'package:smart_search/app/core/shared_widget/common%20widget.dart';
+import 'package:smart_search/app/core/shared_widget/scaffold_wrapper.dart';
+import 'package:smart_search/app/core/utils/colors.dart';
+
+class ScanPart extends StatefulWidget {
+  const ScanPart({super.key});
+
+  @override
+  State<ScanPart> createState() => _ScanPartState();
+}
+
+class _ScanPartState extends State<ScanPart> {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveSizer(
+        builder: (context, orientation, screenType) {
+          return ScaffoldWrapper(
+            appBarTitle: "SCAN/SELECT VEHICLE MODEL​",
+            body: SizedBox(
+                width: 100.w,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    Image.asset(
+                      "assets/images/camera_icon.png",
+                      width: 50.w,
+                      height: 50.w,
+                    ),
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    Center(
+                        child: customButton(
+                            onTap: () {},
+                            width: 70.w,
+                            text: "LONE STAR 2008",
+                            buttonColor: CustomColors.darkGreyColor,
+                            textColor: CustomColors.blackColor)),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    Center(
+                        child: customButton(
+                            onTap: () {},
+                            width: 70.w,
+                            text: "INT. 9900i LT",
+                            buttonColor: CustomColors.darkGreyColor,
+                            textColor: CustomColors.blackColor)),
+                    SizedBox(
+                      height: 2.h,
+                    ),
+                    Center(
+                        child: customButton(
+                            onTap: () {},
+                            width: 70.w,
+                            text: "CUMMINS F2.8/3.8",
+                            buttonColor: CustomColors.darkGreyColor,
+                            textColor: CustomColors.blackColor)),
+                  ],
+                )),
+          );
+        },
+        context: context);
+  }
+}
